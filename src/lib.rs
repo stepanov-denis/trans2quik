b7ce5781d610a0d2aae03cf425c7602b111cfee4
stepanov-denis <stepanov.d.m@yandex.ru>
2025-03-17 00:28:17 +0700

    update example of use Terminal

--- src/lib.rs
+++ src/lib.rs
@@ -382,116 +382,146 @@ impl From<chrono::ParseError> for DateTimeError {
     }
 }
 
-/// The `Terminal` structure is used to interact with the QUIK trading terminal through the library Trans2QUIK.dll.
-///
-/// This structure provides loading of the DLL library Trans2QUIK.dll, establishing a connection to the QUIK terminal
-/// and calling functions from the library to control the terminal and perform trading operations.
-///
-/// # Example of use
-/// Cargo.toml
-/// ```
-/// trans2quik = "1.0.0"
-/// tracing = "0.1.40"
-/// tracing-subscriber = "0.3.18"
-/// lazy_static = "1.5.0"
-/// ```
-/// main.rs
-/// ```
-/// use tracing::info;
-/// use tracing_subscriber;
-/// use lazy_static::lazy_static;
-/// use std::sync::{Arc, Mutex, Condvar};
-/// use std::time::Duration;
-/// use std::error::Error;
-/// use trans2quik;
-///
-/// lazy_static! {
-///     static ref ORDER_CALLBACK_RECEIVED: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
-///     static ref TRADE_CALLBACK_RECEIVED: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
-///     static ref TRANSACTION_CALLBACK_RECEIVED: Arc<(Mutex<bool>, Condvar)> = Arc::new((Mutex::new(false), Condvar::new()));
-/// }
-///
-/// fn main() -> Result<(), Box<dyn Error>> {
-///     tracing_subscriber::fmt::init();
-///
-///     let path = r"c:\QUIK Junior\trans2quik.dll";
-///     let terminal = trans2quik::Terminal::new(path)?;
-///     terminal.connect()?;
-///     terminal.is_dll_connected()?;
-///     terminal.is_quik_connected()?;
-///     terminal.set_connection_status_callback()?;
-///     terminal.set_transactions_reply_callback()?;
-///     let class_code = "QJSIM";
-///     let sec_code = "LKOH";
-///     terminal.subscribe_orders(class_code, sec_code)?;
-///     terminal.subscribe_trades(class_code, sec_code)?;
-///     terminal.start_orders();
-///     terminal.start_trades();
-///     let transaction_str = "ACCOUNT=NL0011100043; CLIENT_CODE=10677; TYPE=L; TRANS_ID=1; CLASSCODE=QJSIM; SECCODE=LKOH; ACTION=NEW_ORDER; OPERATION=B; PRICE=7103,5; QUANTITY=1;";
-///     terminal.send_async_transaction(transaction_str)?;
-///
-///     // Waiting for callback or timeout
-///     {
-///         let order_received = {
-///             let (lock, cvar) = ORDER_CALLBACK_RECEIVED.as_ref();
-///             let received = lock.lock().unwrap();
-///             let timeout = Duration::from_secs(10);
-///
-///             let (received, timeout_result) = cvar
-///                 .wait_timeout_while(received, timeout, |received| !*received)
-///                 .unwrap();
-///   
-///             if timeout_result.timed_out() {
-///                 info!("Timed out waiting for order_status_callback");
-///             }
-///
-///             *received
-///         };
-///
-///         let trade_received = {
-///             let (lock, cvar) = TRADE_CALLBACK_RECEIVED.as_ref();
-///             let received = lock.lock().unwrap();
-///             let timeout = Duration::from_secs(10);
-///
-///             let (received, timeout_result) = cvar
-///                 .wait_timeout_while(received, timeout, |received| !*received)
-///                 .unwrap();
-///
-///             if timeout_result.timed_out() {
-///                 info!("Timed out waiting for trade_status_callback");
-///             }
-///
-///             *received
-///         };
-///
-///         let transaction_received = {
-///             let (lock, cvar) = TRANSACTION_CALLBACK_RECEIVED.as_ref();
-///             let received = lock.lock().unwrap();
-///             let timeout = Duration::from_secs(10);
-///
-///             let (received, timeout_result) = cvar
-///                 .wait_timeout_while(received, timeout, |received| !*received)
-///                 .unwrap();
-///
-///             if timeout_result.timed_out() {
-///                 info!("Timed out waiting for transaction_reply_callback");
-///             }
-///
-///             *received
-///         };
-///
-///         if !order_received && !trade_received && !transaction_received {
-///             info!("Did not receive all expected callbacks");
-///         }
-///     }    
-///
-///     terminal.unsubscribe_orders()?;
-///     terminal.unsubscribe_trades()?;
-///     terminal.disconnect()?;
-///
-///     Ok(())
-/// }
-/// ```
+/**
+The `Terminal` structure is used to interact with the QUIK trading terminal through the library Trans2QUIK.dll.
+
+This structure provides loading of the DLL library Trans2QUIK.dll, establishing a connection to the QUIK terminal
+and calling functions from the library to control the terminal and perform trading operations.
+
+# Example of use
+```
+pub async fn trade(
+    mut command_receiver: mpsc::UnboundedReceiver<AppCommand>,
+    instruments: Arc<RwLock<Vec<Instrument>>>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let path = r"C:\QUIK\trans2quik.dll";
+    let class_code = "";
+    let sec_code = "";
+
+    let terminal = Terminal::new(path)?;
+    let terminal = Arc::new(Mutex::new(terminal));
+    {
+        let terminal_guard = terminal.lock().await;
+        terminal_guard.connect()?;
+        terminal_guard.is_dll_connected()?;
+        terminal_guard.is_quik_connected()?;
+        terminal_guard.set_connection_status_callback()?;
+        terminal_guard.set_transactions_reply_callback()?;
+        terminal_guard.subscribe_orders(class_code, sec_code)?;
+        terminal_guard.subscribe_trades(class_code, sec_code)?;
+        terminal_guard.start_orders();
+        terminal_guard.start_trades();
+    }
+
+    let (transaction_sender, mut transaction_receiver): (
+        UnboundedSender<TransactionInfo>,
+        UnboundedReceiver<TransactionInfo>,
+    ) = mpsc::unbounded_channel();
+
+    {
+        let mut transaction_reply_sender = TRANSACTION_REPLY_SENDER.lock().unwrap();
+        *transaction_reply_sender = Some(transaction_sender);
+    }
+
+    let (order_sender, mut order_receiver): (
+        UnboundedSender<OrderInfo>,
+        UnboundedReceiver<OrderInfo>,
+    ) = mpsc::unbounded_channel();
+
+    {
+        let mut order_status_sender = ORDER_STATUS_SENDER.lock().unwrap();
+        *order_status_sender = Some(order_sender);
+    }
+
+    let (trade_sender, mut trade_receiver): (
+        UnboundedSender<TradeInfo>,
+        UnboundedReceiver<TradeInfo>,
+    ) = mpsc::unbounded_channel();
+
+    {
+        let mut trade_status_sender = TRADE_STATUS_SENDER.lock().unwrap();
+        *trade_status_sender = Some(trade_sender);
+    }
+
+    loop {
+        tokio::select! {
+            Some(command) = command_receiver.recv() => {
+                match command {
+                    AppCommand::Shutdown => {
+                        info!("shutdown signal");
+                        // Access to terminal via Mutex
+                        let terminal_guard = terminal.lock().await;
+
+                        if let Err(err) = terminal_guard.unsubscribe_orders() {
+                            error!("error unsubscribing from orders: {}", err);
+                        }
+                        if let Err(err) = terminal_guard.unsubscribe_trades() {
+                            error!("error unsubscribing from trades: {}", err);
+                        }
+                        if let Err(err) = terminal_guard.disconnect() {
+                            error!("error disconnecting: {}", err);
+                        }
+
+                        info!("shutdown sequence completed");
+                        break;
+                    }
+                }
+            },
+            Some(transaction_info) = transaction_receiver.recv() => {
+                info!("transaction_reply_callback received: {:?}", transaction_info);
+            },
+            Some(order_info) = order_receiver.recv() => {
+                info!("order_status_callback received: {:?}", order_info);
+                if order_info.is_valid() {
+                    // Do something with order info
+                } else {
+                    error!("order_info invalid");
+                }
+            },
+            Some(trade_info) = trade_receiver.recv() => {
+                info!("trade_status_callback received: {:?}", trade_info);
+                if trade_info.is_valid() {
+                    // Do something with trade info
+                } else {
+                    error!("trade_info invalid");
+                }
+            },
+            result = async {
+                if is_trading_time() {
+                    let mut instruments = instruments.write().await;
+                    for instrument in instruments.iter_mut() {
+                            // Get access to the terminal
+                            let terminal_guard = terminal.lock().await;
+                            // Trade here
+                    }
+                } else {
+                    info!("trading is paused, waiting for the next interval to check trading availability");
+                }
+
+                // Pause before the next iteration
+                info!("sleep 60 seconds");
+                sleep(Duration::from_secs(60)).await;
+
+                Ok::<(), Box<dyn Error + Send + Sync>>(())
+            } => {
+                match result {
+                    Ok(_) => {
+                        // Everything went well, we continue the cycle
+                    }
+                    Err(err) => {
+                        // Error Handling
+                        error!("bot error: {}", err);
+                        break;
+                    }
+                }
+            },
+        }
+    }
+
+    Ok(())
+}
+```
+*/
 pub struct Terminal {
     path_to_quik: String,
 
